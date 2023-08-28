29753ba5f715d526a79ef8880b8d29811e7acc62
Xaviour Greenhalgh <xaviour@squareiq.com.au>
2023-08-28 20:39:11 +1000

    implement the worlds worst parallax

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+    }}>
+     <Parallax pages={2} style={{width: '100%', top: '0', left: '0'}}>
+      <ParallaxLayer offset={0.25} speed={1} style={{
+        backgroundImage: 'url("https://media.discordapp.net/attachments/1140589185221275770/1145665665257189376/books_layer_1-removebg-preview.png?width=1332&height=750")',
+      }}></ParallaxLayer>
+
+      <ParallaxLayer offset={0.3} speed={2} style={{
+        backgroundImage: 'url("https://media.discordapp.net/attachments/1140589185221275770/1145665665659830334/books_layer_2-removebg-preview.png?width=1332&height=750")',
+      }}></ParallaxLayer>
+
+      <ParallaxLayer offset={0.5} speed={2}>
+        <h1>
+          Xaviours Blog
+        </h1>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0.35} speed={3} style={{
+        backgroundImage: 'url("https://media.discordapp.net/attachments/1140589185221275770/1145665666100236388/books_layer_3-removebg-preview.png?width=1332&height=750")',
+      }}></ParallaxLayer>
+     </Parallax>
+    </div>
   )
 }
 
